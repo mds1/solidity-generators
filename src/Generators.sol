ff423d27314797a9782c8a8d5abb02fc6066b115
Matt Solomon <matt@mattsolomon.dev>
2022-09-24 07:09:16 -0700

    style: forge fmt

--- src/Generators.sol
+++ src/Generators.sol
@@ -2,20 +2,22 @@
 pragma solidity ^0.8.0;
 
 /**
- * @dev Library to generate evenly spaced arrays between the specified start and stop values,
- * inclusive. If start is greater than stop, the array will be in ascending order. If stop is
- * greater than start, the array will be in descending order. If the start and stop values are the
- * same, the array will contain a single element of that value.
+ * @dev Library to generate evenly spaced arrays between the specified start and
+ * stop values, inclusive. If start is greater than stop, the array will be in
+ * ascending order. If stop is greater than start, the array will be in
+ * descending order. If the start and stop values are the same, the array will
+ * contain a single element of that value.
  *
- * Generating the array by specifying number of elements is the typical API used in other
- * languages, so this is the default. However, there are also methods that let you specify the
- * step size between elements instead.
+ * Generating the array by specifying number of elements is the typical API
+ * used in other languages, so this is the default. However, there are also
+ * methods that let you specify the step size between elements instead.
  *
- * Although the start and stop values are inclusive, the stop value is not guaranteed to be in the
- * array depending on the specified parameters. For example, `linspace(1, 10, 3)` will return `[1, 5, 9]`.
+ * Although the start and stop values are inclusive, the stop value is not
+ * guaranteed to be in the array depending on the specified parameters. For
+ * example, `linspace(1, 10, 3)` will return `[1, 5, 9]`.
  *
- * Both signed and unsigned integers are supported for linearly spaced arrays. Logarithmically
- * spaced arrays are not yet supported.
+ * Both signed and unsigned integers are supported for linearly spaced arrays.
+ * Logarithmically spaced arrays are not yet supported.
  */
 library Generators {
   // --------------------------------
@@ -24,48 +26,66 @@ library Generators {
 
   // -------- By number of elements --------
 
-  function linspace(uint256 start, uint256 stop) internal pure returns (uint256[] memory result) {
+  function linspace(uint start, uint stop)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
     result = linspace(start, stop, 50);
   }
 
-  function linspace(int256 start, int256 stop) internal pure returns (int256[] memory result) {
+  function linspace(int start, int stop)
+    internal
+    pure
+    returns (int[] memory result)
+  {
     result = linspace(start, stop, 50);
   }
 
-  function linspace(uint256 start, uint256 stop, uint256 num) internal pure returns (uint256[] memory result) {
+  function linspace(uint start, uint stop, uint num)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
     bool descending = start > stop;
     if (descending) (start, stop) = (stop, start);
 
-    uint256 size = stop - start;
+    uint size = stop - start;
     if (size == 0) num = 1;
     require(num - 1 <= size, "num larger than range"); // Prevent step size of 0 when num > 1
-    uint256 step = num == 1 ? 0 : size / (num - 1);
+    uint step = num == 1 ? 0 : size / (num - 1);
 
     result = new uint256[](num);
-    for (uint256 i = 0; i < num; i++) {
+    for (uint i = 0; i < num; i++) {
       result[i] = descending ? stop - i * step : start + i * step;
     }
   }
 
-  function linspace(int256 start, int256 stop, uint256 num) internal pure returns (int256[] memory result) {
+  function linspace(int start, int stop, uint num)
+    internal
+    pure
+    returns (int[] memory result)
+  {
     bool descending = start > stop;
     if (descending) (start, stop) = (stop, start);
 
-    uint256 size = range(start, stop);
+    uint size = range(start, stop);
     if (size == 0) num = 1;
     require(num - 1 <= size, "num larger than range"); // Prevent step size of 0 when num > 1
-    uint256 step = num == 1 ? 0 : size / (num - 1);
+    uint step = num == 1 ? 0 : size / (num - 1);
 
     result = new int256[](num);
-    for (uint256 i = 0; i < num; i++) {
-      // Unlike the unsigned case, the signed case requires that we add `step` to the prior array value instead of
-      // simply using `start + i * step`. This is because `i * step` may be larger than type(int256).max for
-      // ranges such as `linspace(type(int256).min, 1, 100)`, and we can't represent that gap as a uint256 and
-      // add the uint256 to an int256.
+    for (uint i = 0; i < num; i++) {
+      // Unlike the unsigned case, the signed case requires that we add `step`
+      // to the prior array value instead of simply using `start + i * step`.
+      // This is because `i * step` may be larger than type(int256).max for
+      // ranges such as `linspace(type(int256).min, 1, 100)`, and we can't
+      // represent that gap as a uint256 and add the uint256 to an int256.
       if (i == 0) {
         result[0] = descending ? stop : start;
       } else {
-        result[i] = descending ? result[i-1] - int256(step)  : result[i-1] + int256(step);
+        result[i] =
+          descending ? result[i - 1] - int(step) : result[i - 1] + int(step);
       }
     }
   }
@@ -73,22 +93,38 @@ library Generators {
   // -------- By step size --------
 
   // Default to a step size of 1.
-  function arange(uint256 start, uint256 stop) internal pure returns (uint256[] memory result) {
+  function arange(uint start, uint stop)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
     result = arange(start, stop, 1);
   }
 
-  function arange(int256 start, int256 stop) internal pure returns (int256[] memory result) {
+  function arange(int start, int stop)
+    internal
+    pure
+    returns (int[] memory result)
+  {
     result = arange(start, stop, 1);
   }
 
   // Specify the step size.
-  function arange(uint256 start, uint256 stop, uint256 step) internal pure returns (uint256[] memory result) {
-    uint256 num = (range(start, stop) / step) + 1;
+  function arange(uint start, uint stop, uint step)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
+    uint num = (range(start, stop) / step) + 1;
     result = linspace(start, stop, num);
   }
 
-  function arange(int256 start, int256 stop, uint256 step) internal pure returns (int256[] memory result) {
-    uint256 num = (range(start, stop) / step) + 1;
+  function arange(int start, int stop, uint step)
+    internal
+    pure
+    returns (int[] memory result)
+  {
+    uint num = (range(start, stop) / step) + 1;
     result = linspace(start, stop, num);
   }
 
@@ -97,20 +133,33 @@ library Generators {
   // -------------------------------------
 
   // Default to 100 elements.
-  function logspace(uint256 start, uint256 stop) internal pure returns (uint256[] memory result) {
+  function logspace(uint start, uint stop)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
     result = logspace(start, stop, 50);
   }
 
   // Specify the number of elements.
-  function logspace(uint256 start, uint256 stop, uint256 num) internal pure returns (uint256[] memory result) {
+  function logspace(uint start, uint stop, uint num)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
     result = logspace(start, stop, num, 10);
   }
 
   // Specify the number of elements and the exponent base.
-  function logspace(uint256 start, uint256 stop, uint256 num, uint256 base) internal pure returns (uint256[] memory result) {
-    // TODO Improve robustness so `linspace(2, 3, 4, 10)` returns [100, 215, 464, 1000] instead of reverting.
+  function logspace(uint start, uint stop, uint num, uint base)
+    internal
+    pure
+    returns (uint[] memory result)
+  {
+    // TODO Improve robustness so `linspace(2, 3, 4, 10)` returns
+    // [100, 215, 464, 1000] instead of reverting.
     result = linspace(start, stop, num);
-    for (uint256 i = 0; i < result.length; i++) {
+    for (uint i = 0; i < result.length; i++) {
       result[i] = base ** result[i];
     }
   }
@@ -119,19 +168,19 @@ library Generators {
   // -------- Utils --------
   // -----------------------
 
-  function range(uint256 a, uint256 b) internal pure returns (uint256 c) {
+  function range(uint a, uint b) internal pure returns (uint c) {
     c = a > b ? a - b : b - a;
   }
 
-  function range(int256 a, int256 b) internal pure returns (uint256 c) {
+  function range(int a, int b) internal pure returns (uint c) {
     bool sameSign = a >= 0 && b >= 0 || a < 0 && b < 0;
     c = sameSign ? range(abs(a), abs(b)) : abs(a) + abs(b);
   }
 
-  function abs(int256 a) internal pure returns (uint256 b) {
+  function abs(int a) internal pure returns (uint b) {
     // Unchecked is required to handle the case when n = type(int256).min
     unchecked {
-      b = uint256(a >= 0 ? a : -a);
+      b = uint(a >= 0 ? a : -a);
     }
   }
 }
--- test/Generators.t.sol
+++ test/Generators.t.sol
@@ -5,26 +5,26 @@ import "forge-std/Test.sol";
 import "src/Generators.sol";
 
 contract GeneratorsTest is Test {
-  event log_named_array(string key, uint256[] val);
-  event log_named_array(string key, int256[] val);
+  event log_named_array(string key, uint[] val);
+  event log_named_array(string key, int[] val);
 
   // Test vectors.
-  uint256[] unsignedExpectedAscending = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  uint256[] unsignedExpectedDescending = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+  uint[] unsignedExpectedAscending = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  uint[] unsignedExpectedDescending = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
-  int256[] signedExpectedAscending = [int256(-5), -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
-  int256[] signedExpectedDescending = [int256(5), 4, 3, 2, 1, 0, -1, -2, -3, -4, -5];
+  int[] signedExpectedAscending = [int(-5), -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+  int[] signedExpectedDescending = [int(5), 4, 3, 2, 1, 0, -1, -2, -3, -4, -5];
 
-  int256[] signedExpectedAscending2 = [int256(0), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  int256[] signedExpectedDescending2 = [int256(10), 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+  int[] signedExpectedAscending2 = [int(0), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  int[] signedExpectedDescending2 = [int(10), 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
-  uint256[] logspaceAscending = [1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6];
-  uint256[] logspaceDescending = [1e6, 1e5, 1e4, 1e3, 1e2, 1e1, 1e0];
+  uint[] logspaceAscending = [1, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6];
+  uint[] logspaceDescending = [1e6, 1e5, 1e4, 1e3, 1e2, 1e1, 1];
 
-  uint256[] logspaceAscending2 = [4, 8, 16, 32, 64, 128, 256, 512, 1024];
-  uint256[] logspaceDescending2 = [1024, 512, 256, 128, 64, 32, 16, 8, 4];
+  uint[] logspaceAscending2 = [4, 8, 16, 32, 64, 128, 256, 512, 1024];
+  uint[] logspaceDescending2 = [1024, 512, 256, 128, 64, 32, 16, 8, 4];
 
-  function assertEq(uint256[] memory a, uint256[] memory b) internal {
+  function assertEq(uint[] memory a, uint[] memory b) internal {
     if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
       emit log("Error: a == b not satisfied [string]");
       emit log_named_array("  Expected", b);
@@ -33,7 +33,7 @@ contract GeneratorsTest is Test {
     }
   }
 
-  function assertEq(int256[] memory a, int256[] memory b) internal {
+  function assertEq(int[] memory a, int[] memory b) internal {
     if (keccak256(abi.encode(a)) != keccak256(abi.encode(b))) {
       emit log("Error: a == b not satisfied [string]");
       emit log_named_array("  Expected", b);
@@ -45,152 +45,156 @@ contract GeneratorsTest is Test {
 
 contract UnsignedLinspace is GeneratorsTest {
   function test_Ascending() public {
-    uint256[] memory array = Generators.linspace(uint256(0), 10, 11);
+    uint[] memory array = Generators.linspace(uint(0), 10, 11);
     assertEq(array, unsignedExpectedAscending);
   }
 
   function test_Descending() public {
-    uint256[] memory array = Generators.linspace(uint256(10), 0, 11);
+    uint[] memory array = Generators.linspace(uint(10), 0, 11);
     assertEq(array, unsignedExpectedDescending);
   }
 
   function test_StartEqualsStop() public {
-    uint256[] memory array = Generators.linspace(uint256(25), 25);
+    uint[] memory array = Generators.linspace(uint(25), 25);
     assertEq(array.length, 1);
     assertEq(array[0], 25);
   }
 
-  function testFuzz_Overloads(uint256 start, uint256 stop) public {
+  function testFuzz_Overloads(uint start, uint stop) public {
     // Bound minimum difference to avoid reverting from num - 1 <= size.
     vm.assume(Generators.range(start, stop) >= 49); // Using 49 since default size is 50.
 
-    uint256[] memory a = Generators.linspace(start, stop);
-    uint256[] memory b = Generators.linspace(start, stop, 50);
+    uint[] memory a = Generators.linspace(start, stop);
+    uint[] memory b = Generators.linspace(start, stop, 50);
     assertEq(a, b);
   }
 }
 
 contract UnsignedArange is GeneratorsTest {
   function test_Ascending() public {
-    uint256[] memory array = Generators.arange(uint256(0), 10, 1);
+    uint[] memory array = Generators.arange(uint(0), 10, 1);
     assertEq(array, unsignedExpectedAscending);
   }
 
   function test_Descending() public {
-    uint256[] memory array = Generators.arange(uint256(10), 0, 1);
+    uint[] memory array = Generators.arange(uint(10), 0, 1);
     assertEq(array, unsignedExpectedDescending);
   }
 
   function test_StartEqualsStop() public {
-    uint256[] memory array = Generators.arange(uint256(25), 25);
+    uint[] memory array = Generators.arange(uint(25), 25);
     assertEq(array.length, 1);
     assertEq(array[0], 25);
   }
 
-  function testFuzz_Overloads(uint256 start, uint256 stop) public {
+  function testFuzz_Overloads(uint start, uint stop) public {
     // Bound max difference to avoid unrealistic/long-running test cases.
     if (stop > start && stop - start > 1000) start = stop - 1000;
     if (stop < start && start - stop > 1000) stop = start - 1000;
 
-    uint256[] memory a = Generators.arange(start, stop);
-    uint256[] memory b = Generators.arange(start, stop, 1);
+    uint[] memory a = Generators.arange(start, stop);
+    uint[] memory b = Generators.arange(start, stop, 1);
     assertEq(a, b);
   }
 }
 
 contract SignedLinspace is GeneratorsTest {
   function test_Ascending() public {
-    int256[] memory array = Generators.linspace(-5, 5, 11);
+    int[] memory array = Generators.linspace(-5, 5, 11);
     assertEq(array, signedExpectedAscending);
 
-    int256[] memory array2 = Generators.linspace(int256(0), 10, 11);
+    int[] memory array2 = Generators.linspace(int(0), 10, 11);
     assertEq(array2, signedExpectedAscending2);
   }
 
   function test_Descending() public {
-    int256[] memory array = Generators.linspace(5, -5, 11);
+    int[] memory array = Generators.linspace(5, -5, 11);
     assertEq(array, signedExpectedDescending);
 
-    int256[] memory array2 = Generators.linspace(int256(10), 0, 11);
+    int[] memory array2 = Generators.linspace(int(10), 0, 11);
     assertEq(array2, signedExpectedDescending2);
   }
 
   function test_StartEqualsStop() public {
-    int256[] memory array = Generators.linspace(int256(25), 25);
+    int[] memory array = Generators.linspace(int(25), 25);
     assertEq(array.length, 1);
     assertEq(array[0], 25);
 
-    int256[] memory array2 = Generators.linspace(-20, -20);
+    int[] memory array2 = Generators.linspace(-20, -20);
     assertEq(array2.length, 1);
     assertEq(array2[0], -20);
   }
 
-  function testFuzz_Overloads(int256 start, int256 stop) public {
+  function testFuzz_Overloads(int start, int stop) public {
     // Bound minimum difference to avoid reverting from num - 1 <= size.
     vm.assume(Generators.range(start, stop) >= 49); // Using 49 since default size is 50.
 
-    int256[] memory a = Generators.linspace(start, stop);
-    int256[] memory b = Generators.linspace(start, stop, 50);
+    int[] memory a = Generators.linspace(start, stop);
+    int[] memory b = Generators.linspace(start, stop, 50);
     assertEq(a, b);
   }
 }
 
 contract SignedArange is GeneratorsTest {
   function test_Ascending() public {
-    int256[] memory array = Generators.arange(-5, 5, 1);
+    int[] memory array = Generators.arange(-5, 5, 1);
     assertEq(array, signedExpectedAscending);
   }
 
   function test_Descending() public {
-    int256[] memory array = Generators.arange(5, -5, 1);
+    int[] memory array = Generators.arange(5, -5, 1);
     assertEq(array, signedExpectedDescending);
   }
 
   function test_StartEqualsStop() public {
-    int256[] memory array = Generators.arange(int256(25), 25);
+    int[] memory array = Generators.arange(int(25), 25);
     assertEq(array.length, 1);
     assertEq(array[0], 25);
 
-    int256[] memory array2 = Generators.arange(-20, -20);
+    int[] memory array2 = Generators.arange(-20, -20);
     assertEq(array2.length, 1);
     assertEq(array2[0], -20);
   }
 
-  function testFuzz_Overloads(int256 start, int256 stop) public {
+  function testFuzz_Overloads(int start, int stop) public {
     // Bound max difference to avoid unrealistic/long-running test cases.
-    if (stop > start && Generators.range(start, stop) > 1000) start = stop - 1000;
-    if (stop < start && Generators.range(start, stop) > 1000) stop = start - 1000;
+    if (stop > start && Generators.range(start, stop) > 1000) {
+      start = stop - 1000;
+    }
+    if (stop < start && Generators.range(start, stop) > 1000) {
+      stop = start - 1000;
+    }
 
-    int256[] memory a = Generators.arange(start, stop);
-    int256[] memory b = Generators.arange(start, stop, 1);
+    int[] memory a = Generators.arange(start, stop);
+    int[] memory b = Generators.arange(start, stop, 1);
     assertEq(a, b);
   }
 }
 
 contract Logspace is GeneratorsTest {
   function test_Ascending() public {
-    uint256[] memory array = Generators.logspace(0, 6, 7); // Base 10.
+    uint[] memory array = Generators.logspace(0, 6, 7); // Base 10.
     assertEq(array, logspaceAscending);
 
-    uint256[] memory array2 = Generators.logspace(2, 10, 10 - 2 + 1, 2); // Base 2.
+    uint[] memory array2 = Generators.logspace(2, 10, 10 - 2 + 1, 2); // Base 2.
     assertEq(array2, logspaceAscending2);
   }
 
   function test_Descending() public {
-    uint256[] memory array = Generators.logspace(6, 0, 7); // Base 10.
+    uint[] memory array = Generators.logspace(6, 0, 7); // Base 10.
     assertEq(array, logspaceDescending);
 
-    uint256[] memory array2 = Generators.logspace(10, 2, 10 - 2 + 1, 2); // Base 2.
+    uint[] memory array2 = Generators.logspace(10, 2, 10 - 2 + 1, 2); // Base 2.
     assertEq(array2, logspaceDescending2);
   }
 
-  function testFuzz_Overloads(uint256 start, uint256 stop) public {
+  function testFuzz_Overloads(uint start, uint stop) public {
     // Bound numbers to prevent overflow.
     start = bound(start, 0, 10);
     stop = bound(stop, start + 49, start + 49 + 20); // Ensure we don't revert due to zero step size.
 
-    uint256[] memory a = Generators.logspace(start, stop);
-    uint256[] memory b = Generators.logspace(start, stop, 50, 10);
+    uint[] memory a = Generators.logspace(start, stop);
+    uint[] memory b = Generators.logspace(start, stop, 50, 10);
     assertEq(a, b);
   }
 }
