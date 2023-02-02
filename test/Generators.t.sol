5d6f128cf4faa91202da7a85be79a91f0aec2f44
Paul Razvan Berg <hello@paulrberg.com>
2023-02-02 14:49:16 +0200

    refactor: use relative paths to import from "src"
    chore: delete "remappings" config option from Foundry config
    style: format "foundry.toml" with Taplo

--- test/Generators.t.sol
+++ test/Generators.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
-import {linspace, arange, logspace, range} from "src/Generators.sol";
+import {linspace, arange, logspace, range} from "../src/Generators.sol";
 
 // =======================
 // ======== Setup ========
